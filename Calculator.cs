426b9f1bf3d6c485904d284709e97ddecec7ac99
connorjohnson85 <connorjohnson853@gmail.com>
2020-10-27 19:03:02 -0600

    starting Calculator Project

---
+++ Calculator.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Calculator {
+    class program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to my Calculator");
+        }
+    }
+}
+
